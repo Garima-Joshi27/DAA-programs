e7974744e162781e3d13b2ee5eea929163a9a03e
Garima-Joshi27 <128968914+Garima-Joshi27@users.noreply.github.com>
2023-03-31 19:40:34 +0530

    Add files via upload

---
+++ week1-I.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+
+
+
+void lenear_search(int arr[], int n, int key)
+{
+
+    for(int i=0 ; i<=n-1 ; i++)
+    {
+        if(arr[i]==key)
+        {
+            printf("key found at index %d",i);
+        }
+        else
+        {
+            printf("key not found");
+        }
+    }
+    
+}
+
+int main()
+{
+    int arr[10];
+    int n , i,key;
+
+    printf("enter the value of n\n");
+    scanf("%d",&n);
+
+    printf("enter array elements\n");
+    for(i=0 ; i<=n-1 ; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+
+ printf("enter key");
+ scanf("%d",&key);
+    lenear_search(arr,n,key);
+ 
+ return 0;
+
+}
